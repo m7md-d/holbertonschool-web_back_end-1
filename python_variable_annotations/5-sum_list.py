417b8292a13ccb6a1f9ac2bbd19979bb71649954
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 09:10:49 +0300

    Add utility function to calculate the sum of a list of floating-point numbers

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""Define a typed utility function for concatenating two strings."""
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of a list of floating-point numbers."""
+    return sum(input_list)
