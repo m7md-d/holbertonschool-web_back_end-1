1e934e11257560b1a435828b60e86973e14980fd
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 10:30:55 +0300

    Refactor element_length function to accept Iterable of Sequence and update type hints

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Define typed utility function for getting the length of list of strings."""
-from typing import List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
