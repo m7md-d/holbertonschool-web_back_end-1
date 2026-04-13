3c423941e6c4f6fb3646205f5c7476106a47496b
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-04-13 09:14:49 +0300

    Fix function definition and update docstring for sum_list utility function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Define a typed utility function for concatenating two strings."""
-def sum_list(input_list: list[float]) -> float:
+"""Define a typed utility function for summing a list of floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of floating-point numbers."""
     return sum(input_list)
