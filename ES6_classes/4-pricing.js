c289fee3945bdd72e3451372fdcad2bd3bcb8c04
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-05-08 13:01:14 +0300

    create: refactor getSumOfHoods to arrow function and add Pricing class for price management

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import { Currency } from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, Currency) {
+    this._amount = amount;
+    this._currency = Currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency}`;
+  }
+
+  static convertPrice(amount, Currency) {
+    return amount * Currency.code;
+  }
+}
