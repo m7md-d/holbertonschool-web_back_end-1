905687f463bc14d60156d6663ca3b1ce9707bebd
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-05-08 21:26:43 +0300

    fix: ensure abstract Building class cannot be instantiated directly

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,5 +1,10 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new TypeError(
+        "Abstract class 'Building' cannot be instantiated directly.",
+      );
+    }
     if (this.evacuationWarningMessage === undefined) {
       throw new TypeError(
         'Class extending Building must override evacuationWarningMessage',
