237d1f7cbbd49c64f935ee2de849250582004d40
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-05-08 21:25:34 +0300

    create: implement Building class with sqft property and evacuation warning validation

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.evacuationWarningMessage === undefined) {
+      throw new TypeError(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
