a1bcc4d39d7d2d6d9ba73ce2018aee4791db55fb
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-05-15 10:02:02 +0300

    create: add getListStudents function and README file

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents(){
+    return [
+      { id: 1, firstName: "Guillaume", loction: "San Francisco" },
+      { id: 2, firstName: "James", loction: "Columbia" },
+      { id: 5, firstName: "Serena", loction: "San Francisco" },
+    ];
+}
